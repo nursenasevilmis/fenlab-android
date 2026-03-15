831ded9354db3fe71cafa4a3e96b03caf23dfb01
nursenasevilmis <nursenasevilmis91@gmail.com>
2026-03-15 22:28:46 +0300

    base url ağ değiştiği için güncellendi

--- app/src/main/java/com/nursena/fenlab_android/core/Constants.kt
+++ app/src/main/java/com/nursena/fenlab_android/core/Constants.kt
@@ -3,5 +3,5 @@ package com.nursena.fenlab_android.core
 
 object Constants {
     // Backend base URL - emülatör için 10.0.2.2
-    const val BASE_URL = "http://10.0.3.2:8080"
+    const val BASE_URL = "http://192.168.1.108:8080"
 }
