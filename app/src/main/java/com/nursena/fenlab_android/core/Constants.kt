e56e12d99911bbbe24b3db7de5c958970c4734bd
nursenasevilmis <nursenasevilmis91@gmail.com>
2026-04-05 22:46:35 +0300

    backend ip'si eklendi

--- app/src/main/java/com/nursena/fenlab_android/core/Constants.kt
+++ app/src/main/java/com/nursena/fenlab_android/core/Constants.kt
@@ -2,8 +2,11 @@ package com.nursena.fenlab_android.core
 
 object Constants {
     //ağ değiştiğinde sadece bu kısım değiştirilecek
-    const val SERVER_IP = "172.17.26.235"
+    /*const val SERVER_IP = "172.17.26.235"
 
     const val BASE_URL  = "http://$SERVER_IP:8080/"
-    const val MINIO_URL = "http://$SERVER_IP:9000"
+    const val MINIO_URL = "http://$SERVER_IP:9000"*/
+    const val BASE_URL  = "https://fenlab.up.railway.app/"
+    const val MINIO_URL = "https://fenlab.up.railway.app"
+    const val SERVER_IP = "fenlab.up.railway.app"
 }
