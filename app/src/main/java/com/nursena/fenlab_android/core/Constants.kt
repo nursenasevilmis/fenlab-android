bc4602c2e154ab429a47d28021b1f0aff1b140d0
nursenasevilmis <nursenasevilmis91@gmail.com>
2026-04-06 03:42:23 +0300

    IP DEĞİŞTİ

--- app/src/main/java/com/nursena/fenlab_android/core/Constants.kt
+++ app/src/main/java/com/nursena/fenlab_android/core/Constants.kt
@@ -7,6 +7,6 @@ object Constants {
     const val BASE_URL  = "http://$SERVER_IP:8080/"
     const val MINIO_URL = "http://$SERVER_IP:9000"*/
     const val BASE_URL  = "https://fenlab.up.railway.app/"
-    const val MINIO_URL = "https://fenlab.up.railway.app"
+    const val MINIO_URL = "https://minio-fenlab.up.railway.app"
     const val SERVER_IP = "fenlab.up.railway.app"
 }
